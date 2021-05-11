4c18b8bc74558cf951165f2c2c006a67ae50c1d8
Tushar <tusharsingla2822@gmail.com>
2021-05-11 22:45:44 +0530

    Setting-up different routes

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const app = express();
 
+app.get("/",function(req,res){
+    res.send("Hello");
+})
+
+app.get("/contact", function(req,res){
+    res.send("Contcact me on 7017182727");
+})
+
+app.get("/about", function(req,res){
+    res.send("Developed by TUSHAR SINGLA");
+})
+
 app.listen(3000, function(){
     console.log("server started");
 })
