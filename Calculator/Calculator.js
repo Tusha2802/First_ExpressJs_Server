866bc594fa3d46610fab75433e2b1f88a54e44b2
Tushar <tusharsingla2822@gmail.com>
2021-05-11 23:52:33 +0530

    BMI-Calculator

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -8,8 +8,20 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
 })
 
+app.get("/bmiCalculator",function(req,res){
+    res.sendFile(__dirname+"/BmiCalci.html");
+})
+
 app.post("/",function(req,res){
     res.send("" + (Number(req.body.num1) + Number(req.body.num2)));
+})
+
+app.post("/bmiCalculator",function(req,res){
+    var w = Number(req.body.weight);
+    var h = Number(req.body.height);
+    var bmi = w/(h*h);
+    
+    res.send(""+ bmi); 
 })
 
 app.listen(3000, function(){
